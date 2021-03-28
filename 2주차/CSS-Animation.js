1579182d35fb5deb41efbdd15302c1819c5f94f1
Kim Dong Hyun <akainoo123@gmail.com>
2021-03-28 14:41:04 +0900

    feat : 2주차 실습 관련 파일 주석 작성

--- 2주차/CSS-Animation.js
+++ 2주차/CSS-Animation.js
@@ -2,5 +2,8 @@ const btn = document.querySelector("#btn");
 
 btn.addEventListener("click",()=>{
     const target = document.querySelectorAll(".transition")[0];
+    // https://velog.io/@rimu/%EC%9E%90%EB%B0%94%EC%8A%A4%ED%81%AC%EB%A6%BD%ED%8A%B8-classList.add-remove-contains-toggle
+    // classList는 해당 DOM의 Class 배열들을 관리합니다.
+    // 특히 CSS 관련 함수를 처리할 때 용이합니다.
     target.classList.toggle("playing");
 });
--- 2주차/Js-Event.js
+++ 2주차/Js-Event.js
@@ -9,6 +9,9 @@ function onBtnContextMenu(event){
     console.log("우클릭!");
 }
 function onInputKeyDownListener(event){
+    // https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Template_literals
+    // tempelate literal 이라고 하는 JS만의 문법입니다.
+    // string 안에서 변수를 나타내기 용이합니다.
     console.log(`${event.key}가 눌러졌어요!`);
 }
 btn.addEventListener("click",onBtnClickListener);
