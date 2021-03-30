07c7fd4c0a6962febeee34c604269d6adf45c998
mochang2 <qjsrodksro@gmail.com>
2021-03-30 13:17:50 +0900

    revise assignment 1

--- 1주차/과제/자율스터디/이창모/script.js
+++ 1주차/과제/자율스터디/이창모/script.js
@@ -5,6 +5,12 @@ button.addEventListener("click", () => {
     const writtenContent = document.getElementById("written-content").value
     const n = document.getElementsByClassName("applied-content").length
 
+    // let arr = document.getElementsByClassName("applied-content")
+
+    // Array.prototype.forEach.call(arr, (element) => {
+    //     element.innerHTML = writtenContent
+    // })
+
     var step
     for (step = 0; step < n; step++) {
         document.getElementsByClassName("applied-content")[step].innerHTML = writtenContent
